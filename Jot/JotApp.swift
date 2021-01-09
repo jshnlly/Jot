20a8cbddec9fee1ed55832674ee2cce0b7e67235
Josh Nelson <jshnelson4@gmail.com>
2021-01-09 15:41:50 -0500

    Initial Commit

---
+++ Jot/JotApp.swift
@@ -0,0 +1,17 @@
+//
+//  JotApp.swift
+//  Jot
+//
+//  Created by Josh Nelson on 1/9/21.
+//
+
+import SwiftUI
+
+@main
+struct JotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
